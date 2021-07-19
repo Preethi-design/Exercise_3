0a5d01f9685355e9ec4d95f3d16130ad99f28f9f
unknown <preethimadhavan111997@gmail.com>
2021-07-19 22:06:59 +0530

    second commit

---
+++ grade_calculation.py
@@ -0,0 +1,24 @@
+print("Enter Marks Obtained in 5 Subjects: ")
+markOne = int(input())
+markTwo = int(input())
+markThree = int(input())
+markFour = int(input())
+markFive = int(input())
+
+tot = markOne+markTwo+markThree+markFour+markFive
+avg = tot/5
+
+if avg>=91 and avg<=100:
+    print("Your Grade is S")
+elif avg>=81 and avg<91:
+    print("Your Grade is A")
+elif avg>=71 and avg<81:
+    print("Your Grade is B")
+elif avg>=61 and avg<71:
+    print("Your Grade is c")
+elif avg>=51 and avg<61:
+    print("Your Grade is D")
+elif avg>=41 and avg<51:
+    print("Your Grade is E")
+else:
+    print("Invalid Input!")
